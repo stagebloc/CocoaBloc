ee6d18a11db31ee6094006fdb074a5c1d9c3fdab
John Heaton <jheaton.dev@gmail.com>
2014-07-17 11:41:59 -0400

    fuck everything about xcode

---
+++ Static Library/CocoaBloc/Models/SBAccount.h
@@ -0,0 +1,14 @@
+//
+//  SBAccount.h
+//  CocoaBloc
+//
+//  Created by John Heaton on 7/16/14.
+//  Copyright (c) 2014 StageBloc. All rights reserved.
+//
+
+#import "MTLModel.h"
+
+@interface SBAccount : MTLModel <MTLJSONSerializing, MTLManagedObjectSerializing>
+
+
+@end
---
+++ Static Library/CocoaBloc/Models/SBStatus.h
@@ -0,0 +1,13 @@
+//
+//  SBStatus.h
+//  CocoaBloc
+//
+//  Created by John Heaton on 7/16/14.
+//  Copyright (c) 2014 StageBloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBStatus : MTLModel <MTLJSONSerializing, MTLManagedObjectSerializing>
+
+@end
