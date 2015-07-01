fec1b8063b874fb3da52b6f0515f31769f06ce4f
drw86312 <david.warner4@gmail.com>
2015-07-01 09:46:17 -0500

    add SBBanUserActivity

---
+++ Pod/Classes/UIKit/SBBanUserActivity.h
@@ -0,0 +1,13 @@
+//
+//  SBBanUserActivity.h
+//  Pods
+//
+//  Created by David Warner on 7/1/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBBanUserActivity : UIActivity
+
+@end
---
+++ Pod/Classes/UIKit/SBBanUserActivity.m
@@ -0,0 +1,13 @@
+//
+//  SBBanUserActivity.m
+//  Pods
+//
+//  Created by David Warner on 7/1/15.
+//
+//
+
+#import "SBBanUserActivity.h"
+
+@implementation SBBanUserActivity
+
+@end
