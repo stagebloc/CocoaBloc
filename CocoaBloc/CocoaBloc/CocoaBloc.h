fd3cb0a2cf83677852e76a4fbf1723f044d0c8d1
John Heaton <jheaton.dev@gmail.com>
2015-09-30 10:46:17 -0400

    Add project for CB + tests

---
+++ CocoaBloc/CocoaBloc/CocoaBloc.h
@@ -0,0 +1,19 @@
+//
+//  CocoaBloc.h
+//  CocoaBloc
+//
+//  Created by John Heaton on 9/30/15.
+//  Copyright © 2015 StageBloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaBloc.
+FOUNDATION_EXPORT double CocoaBlocVersionNumber;
+
+//! Project version string for CocoaBloc.
+FOUNDATION_EXPORT const unsigned char CocoaBlocVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaBloc/PublicHeader.h>
+
+
