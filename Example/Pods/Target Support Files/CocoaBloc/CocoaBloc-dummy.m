9606d0ccc44dca3bcec11000f1889f2e08e8bffa
John Heaton <jheaton.dev@gmail.com>
2015-09-30 10:46:03 -0400

    Remove files + project
    Nuke all the things

--- Example/Pods/Target Support Files/CocoaBloc/CocoaBloc-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CocoaBloc : NSObject
-@end
-@implementation PodsDummy_CocoaBloc
-@end
--- Example/Pods/Target Support Files/Pods-CocoaBloc/Pods-CocoaBloc-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CocoaBloc : NSObject
-@end
-@implementation PodsDummy_Pods_CocoaBloc
-@end
