5da9ac230234cd92f2c1085f2838f68837491465
drw86312 <david.warner4@gmail.com>
2015-10-06 19:02:11 -0500

    add provider implementation

--- CocoaBloc/Private/CocoaBlocAPI.swift
+++ CocoaBloc/Private/CocoaBlocAPI.swift
@@ -10,7 +10,25 @@ import Foundation
 import Moya
 import ReactiveCocoa
 
-public enum CocoaBlocAPI : MoyaTarget {
+public enum CocoaBlocAPI {
+
+    /**
+    Complete the log in process with an authorization code from StageBloc, usually obtained from SBAuthenticationController.
+
+    - Parameters:
+        - authorizationCode: The authorization code.
+    */
+    case loginWithAuthorizationCode(authorizationCode : String)
+
+    /**
+    Log in a StageBloc user with the given credentials.
+
+    - Parameters:
+        - username: The user's username/email address.
+        - password: The user's password.
+    */
+    case logInWithUsername(username : String,
+        password : String)
 
     /**
     Sign up a new StageBloc user with the given user information and desired credentials.
@@ -84,51 +102,234 @@ public enum CocoaBlocAPI : MoyaTarget {
     */
     case sendPasswordReset(email : String)
 
+    /**
+    Updates user information .
+    - Parameters:
+        - email: email address to send passoword reset.
+    */
+//    case updateAuthenticatedUserWithPhoto(parameters : NSDictionary,
+//        photoData : NSData,
+//        progressSignal : RACSignal)
+//
+//
+//    case updateAuthenticatedUserPhoto(photoData : NSData, progressSignal : RACSignal)
+//
+//    case updateAuthenticatedUserLocation(coordinates, CLLocationCoordinate2D)
 
-    //    /*!
-    //    Update user information methods
-    //    */
-    //    case updateAuthenticatedUserWithPhoto(parameters : NSDictionary,
-    //                                           photoData : NSData,
-    //                                      progressSignal : RACSignal)
-    //    case updateAuthenticatedUserPhoto(photoData : NSData, progressSignal : RACSignal)
-    //
-    //    case updateAuthenticatedUserLocation(coordinates, CLLocationCoordinate2D)
-    //
+}
 
+extension CocoaBlocAPI : MoyaTarget {
 
+    // Base CocoaBlocAPI URL
     public var baseURL: NSURL { return NSURL(string: "https://api.stagebloc.com/v1")! }
-}
 
-func stubbedResponse(filename: String) -> NSData! {
-    @objc class TestClass: NSObject { }
+    // URL path
+    public var path: String {
+        switch self {
 
-    let bundle = NSBundle(forClass: TestClass.self)
-    let path = bundle.pathForResource(filename, ofType: "json")
-    return NSData(contentsOfFile: path!)
-}
+        case
+        .loginWithAuthorizationCode,
+        .logInWithUsername:
+            return "/oauth2/token"
 
-func endpointResolver() -> ((endpoint: Endpoint<CocoaBlocAPI>) -> (NSURLRequest)) {
-    return { (endpoint: Endpoint<CocoaBlocAPI>) -> (NSURLRequest) in
-        let request: NSMutableURLRequest = endpoint.urlRequest.mutableCopy() as! NSMutableURLRequest
-        return request
+        case
+        .signupUser:
+            return "/users"
+
+        case
+        .getCurrentlyAuthenticatedUser:
+            return "/users/me"
+
+            //        case
+            //        .updateAuthenticatedUser,
+            //        .updateAuthenticatedUserPhoto:
+            //            return "users/me"
+            //
+        case
+        .getUser(let userID):
+            return "/users/\(userID)"
+
+        case
+        .sendPasswordReset:
+            return "/users/password/reset"
+            //
+            //        case
+            //        .updateAuthenticatedUserLocation:
+            //            return "users/me/location/update"
+            //
+        case
+        .banUser(let userID, let accountID, _):
+            return "/users/\(userID)/ban/\(accountID)"
+
+        case
+        .getPostedContentFromUser(let userID, let contentListType, _):
+            return "/users/\(userID)/content/\(contentListType)"
+            
+        default :
+            return ""
+        }
+    }
+
+    // Request type
+    public var method: Moya.Method {
+        switch self {
+
+        case
+        .loginWithAuthorizationCode,
+        .logInWithUsername,
+
+        .signupUser,
+        .banUser,
+        .sendPasswordReset:
+            //        .updateAuthenticatedUser,
+            //        .updateAuthenticatedUserPhoto,
+            //        .updateAuthenticatedUserLocation:
+            return .POST
+        case
+        .getCurrentlyAuthenticatedUser,
+        .getUser,
+        .getPostedContentFromUser:
+            return .GET
+
+        default :
+            return .GET
+        }
+    }
+
+    // Sample data
+    public var sampleData: NSData {
+        switch self {
+
+        case
+        .loginWithAuthorizationCode:
+            return "AAA".dataUsingEncoding(NSUTF8StringEncoding)!
+
+        case
+        .logInWithUsername:
+            return "AAA".dataUsingEncoding(NSUTF8StringEncoding)!
+
+        case
+        .signupUser:
+            return "AAA".dataUsingEncoding(NSUTF8StringEncoding)!
+
+        case
+        .getCurrentlyAuthenticatedUser:
+            return "BBB".dataUsingEncoding(NSUTF8StringEncoding)!
+
+        case
+        .getUser:
+            return "BBB".dataUsingEncoding(NSUTF8StringEncoding)!
+
+        case
+        .updateAuthenticatedUserWithParameters:
+            return "CCC".dataUsingEncoding(NSUTF8StringEncoding)!
+
+        case
+        .banUser:
+            return "DDD".dataUsingEncoding(NSUTF8StringEncoding)!
+
+        case
+        .getPostedContentFromUser:
+            return "EEE".dataUsingEncoding(NSUTF8StringEncoding)!
+
+        case
+        .sendPasswordReset:
+            return "FFF".dataUsingEncoding(NSUTF8StringEncoding)!
+
+        default:
+            return "XXX".dataUsingEncoding(NSUTF8StringEncoding)!
+        }
+    }
+
+    // Parameters
+    public var parameters: [String: AnyObject] {
+        switch self {
+
+        case .loginWithAuthorizationCode(let authorizationCode):
+            return ["code" : authorizationCode,
+                "expand" : true,
+                "include_admin_accounts" : true,
+                "grant_type" : "authorization_code"]
+
+        case .logInWithUsername(let username,
+            let password):
+            return ["username" : username,
+                "password" : password,
+                "expand" : "user",
+                "include_admin_accounts" : true,
+                "grant_type" : "password"]
+
+        case .signupUser(let email,
+            let name,
+            let password,
+            let birthday,
+            let gender,
+            let sourceAccountID):
+
+            let df = NSDateFormatter()
+            df.locale = NSLocale(localeIdentifier: "EN_US_POSIX")
+            df.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+            df.dateFormat = "yyyy-MM-dd"
+
+            return ["email" : email,
+                "name" : name,
+                "password" : password,
+                "birthday" : df.stringFromDate(birthday),
+                "gender" : gender,
+                "source_account_id" : sourceAccountID]
+
+        case .banUser(_, _, let reason):
+            return ["reason" : reason]
+
+        case .sendPasswordReset(let email):
+            return ["email" : email]
+
+        default:
+            /*! Endpoints w/o parameters
+
+            .getCurrentlyAuthenticatedUser
+            .getUser
+            */
+            
+            return [:]
+        }
     }
 }
 
-//struct Provider {
-//    private static var endpointsClosure = { (target: CocoaBlocAPI) -> Endpoint<CocoaBlocAPI> in
-//
-//        return endpoint.endpointByAddingParameters(["client_id":"de4346e640860eb3d6fd97e11e475d0d"]).urlRequest
-//
-//
-//        var endpoint: Endpoint<CocoaBlocAPI> = Endpoint<CocoaBlocAPI>(URL: url(target), sampleResponse: .Success(200, {target.sampleData}), method: target.method, parameters: target.parameters)
-//
-//        switch target {
-//        case .XApp:
-//            return endpoint
-//
-//        default:
-//            return endpoint.endpointByAddingHTTPHeaderFields(["X-Xapp-Token": XAppToken().token ?? ""])
-//        }
+struct Provider {
+    private static var endpointsClosure = { (target: CocoaBlocAPI, method: Moya.Method, parameters: [String: AnyObject]) -> Endpoint<CocoaBlocAPI> in
+
+        var endpoint: Endpoint<CocoaBlocAPI> = Endpoint<CocoaBlocAPI>(URL: url(target), sampleResponse: .Success(200, {target.sampleData}), method: method, parameters: parameters)
+
+        switch target {
+
+        case .loginWithAuthorizationCode,
+            .logInWithUsername:
+
+            return endpoint.endpointByAddingParameters(["client_secret" : "blahblahblah"])
+
+        default:
+            return endpoint
+        }
+    }
+}
+
+public func url(route: MoyaTarget) -> String {
+    return route.baseURL.URLByAppendingPathComponent(route.path).absoluteString
+}
+
+//func endpointResolver() -> ((endpoint: Endpoint<CocoaBlocAPI>) -> (NSURLRequest)) {
+//    return { (endpoint: Endpoint<CocoaBlocAPI>) -> (NSURLRequest) in
+//        let request: NSMutableURLRequest = endpoint.urlRequest.mutableCopy() as! NSMutableURLRequest
+//        return request
 //    }
 //}
+
+//func stubbedResponse(filename: String) -> NSData! {
+//    @objc class TestClass: NSObject { }
+//
+//    let bundle = NSBundle(forClass: TestClass.self)
+//    let path = bundle.pathForResource(filename, ofType: "json")
+//    return NSData(contentsOfFile: path!)
+//}
+
