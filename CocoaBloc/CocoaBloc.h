9a316961b23d924418759fc4a2481b6e8c51abfc
John Heaton <jheaton.dev@gmail.com>
2015-10-26 20:49:38 -0400

    Remove comment in umbrella header

--- CocoaBloc/CocoaBloc.h
+++ CocoaBloc/CocoaBloc.h
@@ -32,7 +32,3 @@ FOUNDATION_EXPORT double CocoaBlocVersionNumber;
 
 //! Project version string for CocoaBloc.
 FOUNDATION_EXPORT const unsigned char CocoaBlocVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <CocoaBloc/PublicHeader.h>
-
-
