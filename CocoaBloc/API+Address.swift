8277370dfb7143a68b549815e0a01a80969a52d1
John Heaton <jheaton.dev@gmail.com>
2016-07-17 17:39:47 -0400

    Decoding, AddressType enumeration

--- CocoaBloc/API+Address.swift
+++ CocoaBloc/API+Address.swift
@@ -11,10 +11,43 @@ import Foundation
 import Alamofire
 
 extension API {
+	
+	public enum AddressType: String {
+		case Shipping				= "shipping"
+		case InactiveShipping		= "inactive-shipping"
+		case Location				= "location"
+		case OneTimeShipping		= "one-time-shipping"
+		case PaymentVerification	= "payment-verification"
+		case Billing				= "billing"
+	}
 
 	public static func getAddress(withID addressID: Int) -> Endpoint<Address> {
 		return Endpoint(path: "users/me/addresses/\(addressID)", method: .GET)
 	}
+
+	public static func getAllAddresses(forType type: AddressType) -> Endpoint<[Address]> {
+		return Endpoint(path: "users/me/addresses/\(type)", method: .GET)
+	}
 	
-//	public static func 
+	public static func createAddress(withType type: AddressType) -> Endpoint<Address> {
+		return Endpoint(
+			path: "users/me/addresses/\(type)",
+			method: .POST,
+			parameters: [
+				:
+			])
+	}
+	
+	public static func updateAddress(withType type: AddressType, identifier: Int) -> Endpoint<Address> {
+		return Endpoint(
+			path: "users/me/addresses/\(type)/\(identifier)",
+			method: .POST,
+			parameters: [
+				:
+			])
+	}
+	
+	public static func deleteAddress(withType type: AddressType, identifier: Int) -> Endpoint<Address> {
+		return Endpoint(path: "users/me/addresses/\(type)\(identifier)", method: .DELETE)
+	}
 }
--- CocoaBloc/Models/Cart.swift
+++ CocoaBloc/Models/Cart.swift
@@ -7,6 +7,7 @@
 //
 
 import Argo
+import Curry
 
 public struct Cart: Identifiable, Decodable {
 	
@@ -24,6 +25,23 @@ public struct Cart: Identifiable, Decodable {
 		public let sku: String
 		public let parentID: Int?
 		public let lockExpires: Int
+		
+		public static func decode(json: JSON) -> Decoded<Item> {
+			let a = curry(Item.init)
+				<^> json <| "id"
+				<*> json <| "cart"
+				<*> json <| "created"
+				<*> json <| "hash"
+				<*> json <| "product_id"
+				<*> json <| "product_type"
+				<*> json <| "named_price"
+			return a
+				<*> json <| "quantity"
+				<*> json <| "status"
+				<*> json <| "sku"
+				<*> json <|? "parent_id"
+				<*> json <| "lock_expires"
+		}
 	}
 	
 	public struct Totals: Decodable {
@@ -31,9 +49,17 @@ public struct Cart: Identifiable, Decodable {
 		public let subtotal: Double
 		public let total: Double
 		public let shipping: Double
+		
+		public static func decode(json: JSON) -> Decoded<Totals> {
+			return curry(Totals.init)
+				<^> json <| "items"
+				<*> json <| "subtotal"
+				<*> json <| "total"
+				<*> json <| "shipping"
+		}
 	}
 	
-	public enum Status: String {
+	public enum Status: String, Decodable {
 		case started = "STARTED"
 		case reachedCheckout = "REACHED_CHECKOUT"
 		case completedCheckout = "COMPLETED_CHECKOUT"
@@ -41,13 +67,27 @@ public struct Cart: Identifiable, Decodable {
 	
 	public let identifier: Int
 	public let userID: Int
-	public let user: User?
-	public let creationDate: NSDate
+//	public let user: User?
 	public let sessionID: Int
-	public let emailAddress: String
-	public let
-	public let items: [Item]?
+	public let creationDate: NSDate
+	public let emailAddress: String?
+	public let status: Status
+//	public let items: [Item]?
 	public let shippingAddress: Expandable<Address>
 	public let totals: Totals
-	//	public let shippingDetails:
+//	public let shippingDetails:
+//	public let shippingRates:
+//	public let selectedShipping
+	
+	public static func decode(json: JSON) -> Decoded<Cart> {
+		return curry(Cart.init)
+			<^> json <| "id"
+			<*> json <| "user_id"
+			<*> json <| "session_id"
+			<*> json <| "created"
+			<*> json <|? "email"
+			<*> json <| "status"
+			<*> json <| "shipping_address"
+			<*> json <| "totals"
+	}
 }
